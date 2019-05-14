70cfc7e6b15559bdf99414a382d9226cf4100673
Dennis <dennis.basyrov@gmail.com>
2019-05-14 05:05:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,18 @@
+# def reverse_each_word(string)
+#   array = string.split(" ")
+#   array2 = []
+#   array.each do |i|
+#     array2 << i.reverse
+#   end
+#     new_string = array2.join(" ")
+#     return new_string
+# end  
+
 def reverse_each_word(string)
   array = string.split(" ")
-  array2 = []
-  array.each do |i|
-    array2 << i.reverse
+  array.collect do |i|
+    i.reverse
   end
-    new_string = array2.join(" ")
+    new_string = array.join(" ")
     return new_string
 end  
